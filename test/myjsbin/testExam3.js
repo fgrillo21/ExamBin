c30628020c27db1d327a63e29492c1b7aea0f4c6
trappola_7 <matteo.borghi20@gmail.com>
2014-10-24 16:30:41 +0200

    add file for test

---
+++ test/myjsbin/testExam3.js
@@ -0,0 +1,31 @@
+var chai = require('chai');
+var config = require('./testConfig2');
+
+//TODO pay attention, I NEED  variable WINDOW and not WINDOWS
+var window = config.getExam();
+
+
+describe("Validate Exam", function() {
+    describe("Javascript Rule", function() {
+        it("La variabile i dovrebbe avere il valore 10", function() {
+            chai.expect(window.i).to.equal(10);
+        });
+
+        it("La variabile denominata 'borghi' dovrebbe avere il valore 22", function() {
+            chai.expect(window.borghi).to.equal(22);
+         });
+         it("La funzione z() dovrebbe ritornare il valore 3", function() {
+             chai.expect(window.zeta()).to.equal(3);
+         });
+    });
+    describe("HTML Rule", function() {
+        it("Nessun elemento html dovrebbe contenere l'atrtibuto class", function() {
+            var numElementClass  = window.$("[class]").length;
+            chai.expect(numElementClass).to.equal(0);
+        });
+        it("Nessun elemento html dovrebbe contenere l'atrtibuto id", function() {
+            var numElementId  = window.$("[id]").length;
+            chai.expect(numElementId).to.equal(0);
+        });
+    });
+});
